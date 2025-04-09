94111c9eac81cfa4705229f2e9c02d2ab56b18da
Leandro Faria <lee.lara1219@gmail.com>
2025-04-09 18:21:18 -0500

    fix: add lifecycle test and fix set up

--- test/e2e/logicModules/FundingPotE2E.t.sol
+++ test/e2e/logicModules/FundingPotE2E.t.sol
@@ -13,11 +13,18 @@ import {
     LM_PC_FundingPot_v1,
     ILM_PC_FundingPot_v1
 } from "@lm/LM_PC_FundingPot_v1.sol";
-import {FM_DepositVault_v1} from "@fm/depositVault/FM_DepositVault_v1.sol";
+import {
+    FM_BC_Bancor_Redeeming_VirtualSupply_v1,
+    IFM_BC_Bancor_Redeeming_VirtualSupply_v1
+} from
+    "test/modules/fundingManager/bondingCurve/FM_BC_Bancor_Redeeming_VirtualSupply_v1.t.sol";
+import {PP_Streaming_v2} from "src/modules/paymentProcessor/PP_Streaming_v2.sol";
+
 import {ERC165Upgradeable} from
     "@oz-up/utils/introspection/ERC165Upgradeable.sol";
 import {ERC20Mock} from "test/utils/mocks/ERC20Mock.sol";
 import {SafeERC20} from "@oz/token/ERC20/utils/SafeERC20.sol";
+import {ERC20Issuance_v1} from "@ex/token/ERC20Issuance_v1.sol";
 
 contract FundingPotE2E is E2ETest {
     // Module Configurations for the current E2E test. Should be filled during setUp() call.
@@ -27,6 +34,7 @@ contract FundingPotE2E is E2ETest {
     address contributor1 = makeAddr("contributor 1");
     address contributor2 = makeAddr("contributor 2");
     address contributor3 = makeAddr("contributor 3");
+    ERC20Issuance_v1 issuanceToken;
 
     // Constants
     uint constant _SENTINEL = type(uint).max;
@@ -65,10 +73,11 @@ contract FundingPotE2E is E2ETest {
         );
 
         // PaymentProcessor
-        setUpSimplePaymentProcessor();
+        setUpStreamingPaymentProcessor();
         moduleConfigurations.push(
             IOrchestratorFactory_v1.ModuleConfig(
-                simplePaymentProcessorMetadata, bytes("")
+                streamingPaymentProcessorMetadata,
+                abi.encode(10, 0, 30) // defaultStart, defaultCliff, defaultEnd
             )
         );
 
@@ -79,9 +88,37 @@ contract FundingPotE2E is E2ETest {
                 LM_PC_FundingPot_v1Metadata, abi.encode(contributionToken)
             )
         );
+        setUpBancorVirtualSupplyBondingCurveFundingManager();
+
+        // BancorFormula 'formula' is instantiated in the E2EModuleRegistry
+
+        issuanceToken = new ERC20Issuance_v1(
+            "Bonding Curve Token", "BCT", 18, type(uint).max - 1, address(this)
+        );
+
+        IFM_BC_Bancor_Redeeming_VirtualSupply_v1.BondingCurveProperties memory
+            bc_properties = IFM_BC_Bancor_Redeeming_VirtualSupply_v1
+                .BondingCurveProperties({
+                formula: address(formula),
+                reserveRatioForBuying: 333_333,
+                reserveRatioForSelling: 333_333,
+                buyFee: 0,
+                sellFee: 0,
+                buyIsOpen: true,
+                sellIsOpen: true,
+                initialIssuanceSupply: 10,
+                initialCollateralSupply: 30
+            });
+
+        moduleConfigurations.push(
+            IOrchestratorFactory_v1.ModuleConfig(
+                bancorVirtualSupplyBondingCurveFundingManagerMetadata,
+                abi.encode(address(issuanceToken), bc_properties, token)
+            )
+        );
     }
 
-    function test_e2e_BountyManagerLifecycle() public {
+    function test_e2e_FundingPotLifecycle() public {
         //--------------------------------------------------------------------------
         // Orchestrator_v1 Initialization
         //--------------------------------------------------------------------------
@@ -94,11 +131,18 @@ contract FundingPotE2E is E2ETest {
         IOrchestrator_v1 orchestrator =
             _create_E2E_Orchestrator(workflowConfig, moduleConfigurations);
 
-        FM_DepositVault_v1 fundingManager =
-            FM_DepositVault_v1(address(orchestrator.fundingManager()));
+        IFM_BC_Bancor_Redeeming_VirtualSupply_v1 bondingCurveFundingManager =
+        IFM_BC_Bancor_Redeeming_VirtualSupply_v1(
+            address(orchestrator.fundingManager())
+        );
 
         LM_PC_FundingPot_v1 fundingPot;
 
+        // Define payment processor
+        PP_Streaming_v2 paymentProcessor =
+            PP_Streaming_v2(address(orchestrator.paymentProcessor()));
+
+        // Get modules list
         address[] memory modulesList = orchestrator.listModules();
         for (uint i; i < modulesList.length; ++i) {
             if (
@@ -111,20 +155,124 @@ contract FundingPotE2E is E2ETest {
             }
         }
 
-        // we authorize the deployer of the orchestrator as the fundngPot admin
+        FM_BC_Bancor_Redeeming_VirtualSupply_v1 fundingManager =
+        FM_BC_Bancor_Redeeming_VirtualSupply_v1(
+            address(orchestrator.fundingManager())
+        );
+
+        issuanceToken.setMinter(address(fundingManager), true);
+
+        // 2. Grant FUNDING_POT_ADMIN_ROLE to this contract for configuring rounds
         fundingPot.grantModuleRole(
             fundingPot.FUNDING_POT_ADMIN_ROLE(), address(this)
         );
-        // Funders deposit funds
 
-        // IMPORTANT
-        // =========
-        // Due to how the underlying rebase mechanism works, it is necessary
-        // to always have some amount of tokens in the orchestrator.
-        // It's best, if the owner deposits them right after deployment.
-        uint initialDeposit = 10e18;
-        token.mint(address(this), initialDeposit);
-        token.approve(address(fundingManager), initialDeposit);
-        fundingManager.deposit(initialDeposit);
+        // Configure rounds
+        // Round 1
+        uint64 round1Id = fundingPot.createRound(
+            block.timestamp + 1 days, // start
+            block.timestamp + 30 days, // end
+            1000e18, // cap
+            address(0), // no hook
+            bytes(""), // no hook function
+            false, // auto closure
+            false // no global caps
+        );
+        vm.warp(block.timestamp + 1 days);
+
+        // Add access criteria to round 1
+        address[] memory allowedAddresses = new address[](2);
+        allowedAddresses[0] = contributor1;
+        allowedAddresses[1] = contributor2;
+
+        fundingPot.setAccessCriteriaForRound(
+            round1Id,
+            ILM_PC_FundingPot_v1.AccessCriteria({
+                accessCriteriaType: ILM_PC_FundingPot_v1.AccessCriteriaType.LIST,
+                nftContract: address(0),
+                merkleRoot: bytes32(0),
+                allowedAddresses: allowedAddresses
+            })
+        );
+
+        // Round 2
+        uint64 round2Id = fundingPot.createRound(
+            block.timestamp + 1, // start
+            block.timestamp + 60 days, // end
+            2e18, // cap
+            address(0), // no hook
+            bytes(""), // no hook function
+            false, // auto closure
+            false // no global caps
+        );
+
+        // Add access criteria to round 2
+        allowedAddresses = new address[](1);
+        allowedAddresses[0] = contributor3;
+
+        fundingPot.setAccessCriteriaForRound(
+            round2Id,
+            ILM_PC_FundingPot_v1.AccessCriteria({
+                accessCriteriaType: ILM_PC_FundingPot_v1.AccessCriteriaType.LIST,
+                nftContract: address(0),
+                merkleRoot: bytes32(0),
+                allowedAddresses: allowedAddresses
+            })
+        );
+        fundingPot.setAccessCriteriaPrivileges(
+            round1Id,
+            0, // accessCriteriaId
+            1_000_000_000_000_000_000, // personalCap
+            0, // capByNFT
+            0, // capByMerkle
+            0, // capByList
+            true, // overrideContributionSpan
+            10, // start
+            0, // cliff
+            30 // end
+        );
+        fundingPot.setAccessCriteriaPrivileges(
+            round2Id,
+            0, // accessCriteriaId
+            1_000_000_000_000_000_000, // personalCap
+            0, // capByNFT
+            0, // capByMerkle
+            0, // capByList
+            true, // overrideContributionSpan
+            10, // start
+            0, // cliff
+            30 // end
+        );
+
+        // Fund contributors
+        contributionToken.mint(contributor1, 500e18);
+        contributionToken.mint(contributor2, 500e18);
+        contributionToken.mint(contributor3, 1000e18);
+
+        // Contributors approve funding pot
+        vm.prank(contributor1);
+        contributionToken.approve(address(fundingPot), 500e18);
+        vm.prank(contributor2);
+        contributionToken.approve(address(fundingPot), 500e18);
+        vm.prank(contributor3);
+        contributionToken.approve(address(fundingPot), 1000e18);
+
+        // Contributors contribute to rounds
+        vm.prank(contributor1);
+        fundingPot.contributeToRound(round1Id, 1e18, 0, new bytes32[](0));
+
+        // Trigger payment order creation
+        fundingPot.trigger();
+
+        // Fast forward to after rounds end
+        vm.warp(block.timestamp + 32 days);
+
+        /// Assert a payment order was created
+        PP_Streaming_v2.Stream[] memory streams = paymentProcessor
+            .viewAllPaymentOrders(address(fundingPot), contributor1);
+        assertEq(streams.length, 1);
+
+        // Verify tokens were minted from curve
+        uint totalContributions = 1500e18; // 300 + 200 + 1000
     }
 }
