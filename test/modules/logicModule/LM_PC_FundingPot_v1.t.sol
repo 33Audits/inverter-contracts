3f73963720ad011a2ac5833030cc440f2edb67d3
Zuhaib Mohammed <zzzuhaibmohd@gmail.com>
2025-04-22 12:10:14 +0530

    fix: PR Review fixes#4

--- test/modules/logicModule/LM_PC_FundingPot_v1.t.sol
+++ test/modules/logicModule/LM_PC_FundingPot_v1.t.sol
@@ -2461,7 +2461,7 @@ contract LM_PC_FundingPot_v1_Test is ModuleTest {
     }
 
     // Test exposed internal function closeRound
-    function test_closeRound_WhenCapReached() public {
+    function test_closeRound_worksGivenCapReached() public {
         testCreateRound();
 
         uint64 roundId = fundingPot.getRoundCount();
